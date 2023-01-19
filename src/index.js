5d2cb9dd3edd4bf3de83dfbbb72c2fd05b7451b7
Lilia Vasileva <lilia.vvasileva@gmail.com>
2023-01-19 19:18:55 +0200

    Set up static assets

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 const config = require('./config');
 
-// const setupViewEngine  = require('./config/viewEngine');
-// setupViewEngine(app);
+const setupViewEngine  = require('./config/viewEngine');
+setupViewEngine(app);
 
-require('./config/viewEngine')(app);
+app.use(express.static('src/public'));
+
+// require('./config/viewEngine')(app);
 
 
 app.get('/', (req, res) => {
