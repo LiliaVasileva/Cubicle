2d1833151a6bb61cefac8238fb3ea10a3d4119f9
Lilia Vasileva <lilia.vvasileva@gmail.com>
2023-01-19 19:32:27 +0200

    Adding about page

--- src/index.js
+++ src/index.js
@@ -12,7 +12,11 @@ app.use(express.static('src/public'));
 
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('index');
 });
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
 
 app.listen(config.PORT, ()=> console.log(`Server is running on port ${config.PORT}...`));
