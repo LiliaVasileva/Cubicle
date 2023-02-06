e5aaf52cec404600587ab36c6528cde880179e1b
LiliaVasileva <lilia.vvasileva@gmail.com>
2023-02-06 16:55:56 +0200

    Adding Accessorie items

--- src/controllers/accessoryControlller.js
+++ src/controllers/accessoryControlller.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
+const Accessory = require('../models/Accessory')
 
+router.get('/create', (req, res) => {
+    res.render('createAccessory')
 
-router.get('/create',(req, res) => {
+});
+
+router.post('/create', async (req, res) => {
+    const { name, imageUrl, description } = req.body;
+
+    const accessory = await new Accessory({name, imageUrl, description});
     
-})
+    // await Accessory.create({ame, imageUrl, description}}); one more way to creare items
+
+    accessory.save();
+
+    res.redirect('/')
+
+});
 
 module.exports = router;
